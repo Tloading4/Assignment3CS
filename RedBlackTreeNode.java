3877530e6d0828eb9a00dd52ff0d3b359ca7a6c0
Travis Pryor <151086260+Tloading4@users.noreply.github.com>
2024-11-16 14:47:52 -0600

    Create RedBlackTreeNode.java

---
+++ RedBlackTreeNode.java
@@ -0,0 +1,10 @@
+public class RedBlackTreeNode {
+    Product product;
+    RedBlackTreeNode left, right, parent;
+    boolean color; // true for red, false for black
+
+    public RedBlackTreeNode(Product product) {
+        this.product = product;
+        this.color = true; // new nodes are red by default
+    }
+}
